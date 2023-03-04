2077ee6c56cc865e2088bb048f5b65e15297b047
Pietro Giordano <giopi87@gmail.com>
2023-03-04 17:12:01 +0100

    inizializzazione progetto

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+      plugins: [vue()],
+      css: {
+            preprocessorOptions: {
+                  scss: {
+                        additionalData: `
+                                                @import './src/styles/partials/variables.scss';
+                                                @import './src/styles/partials/mixins.scss';
+                                                `
+
+                  }
+            }
+      }
+})
