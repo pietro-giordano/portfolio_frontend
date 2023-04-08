6f2b142f179460b45d7652cbd52831e9a48eb3dd
Pietro Giordano <giopi87@gmail.com>
2023-04-08 17:55:29 +0200

    aggiunta link social

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -11,23 +11,23 @@ export default {
                   social: [
                         {
                               name: 'facebook',
-                              icon: '<font-awesome-icon icon="fa-brands fa-facebook" />',
+                              icon: 'fa-brands fa-facebook',
                               link: '#'
                         }, {
                               name: 'instagram',
-                              icon: '<font-awesome-icon icon="fa-brands fa-instagram" />',
+                              icon: 'fa-brands fa-instagram',
                               link: '#'
                         }, {
                               name: 'linkedin',
-                              icon: '<font-awesome-icon icon="fa-brands fa-linkedin" />',
+                              icon: 'fa-brands fa-linkedin',
                               link: '#'
                         }, {
                               name: 'discord',
-                              icon: '<font-awesome-icon icon="fa-brands fa-discord" />',
+                              icon: 'fa-brands fa-discord',
                               link: '#'
                         }, {
                               name: 'github',
-                              icon: '<font-awesome-icon icon="fa-brands fa-github" />',
+                              icon: 'fa-brands fa-github',
                               link: '#'
                         },
                   ],
@@ -41,24 +41,25 @@ export default {
             <Navbar />
 
             <div id="jumbotron" class="h-screen container mx-auto grid grid-cols-2">
-                  <div class="mt-40 text-orange-very border-2 border-white">
-                        <div class="font-bold text-4xl leading-relaxed">
+                  <div class="mt-40 border-2 border-white">
+                        <div class="font-bold text-4xl leading-relaxed text-orange-very">
                               Hi, It's me
                               <h1 class="text-6xl text-orange-light">Pietro Giordano</h1>
                               And I'm a
                               <h2 class="text-orange">Jr. Full Stack Web Developer</h2>
                         </div>
 
-                        <p class="py-5">
+                        <p class="py-5 text-orange-very">
                               Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, reprehenderit consequatur?
                               Itaque vel quisquam nulla assumenda? Tenetur aut fugiat soluta?
                         </p>
 
                         <div>
-                              <ul>
-                                    <li v-for="item in social">
-                                          <a href="">
-                                                <font-awesome-icon icon="`${{ item.icon }}`" />
+                              <ul class="flex">
+                                    <li v-for="item in social"
+                                          class="flex justify-center items-center w-12 h-12 text-orange-light border-2 border-orange-light rounded-full me-3 hover:text-orange hover:border-orange">
+                                          <a :href="item.link" class="p-2">
+                                                <font-awesome-icon :icon="`${item.icon}`" size="xl" />
                                           </a>
                                     </li>
                               </ul>
