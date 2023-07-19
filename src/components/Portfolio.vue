8da7d759f0c81bf760c842cff6c62d86c04a9304
Pietro Giordano <giopi87@gmail.com>
2023-07-19 17:02:53 +0200

    avanzamento projects

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,5 +1,6 @@
 <script>
 import axios from 'axios';
+import scrollreveal from 'scrollreveal';
 
 export default {
       name: 'Portfolio',
@@ -15,6 +16,17 @@ export default {
                         console.log(response.data);
                         this.projects = response.data.projects;
                   })
+
+            scrollreveal({
+                  reset: true,
+                  distance: '80px',
+                  duration: 2000,
+                  delay: 200
+            })
+            scrollreveal().reveal(this.$refs.bottom, { origin: 'bottom' });
+            scrollreveal().reveal(this.$refs.top, { origin: 'top' });
+            scrollreveal().reveal(this.$refs.left, { origin: 'left' });
+            scrollreveal().reveal(this.$refs.right, { origin: 'right' });
       }
 }
 </script>
@@ -22,29 +34,40 @@ export default {
 <template>
       <section id="portfolio" class="container mx-auto pt-14 md:pt-20 lg:pt-28">
 
-            <h2 ref="top" class="sm text-orange-very text-lg pb-4 text-center">Latest <span class="text-orange">Project</span>
+            <h2 ref="right" class="sm text-orange-very text-lg pb-4 text-center">Latest <span
+                        class="text-orange">Project</span>
             </h2>
 
-            <div v-for="project in projects">
-                  <div class="grid grid-cols-3 border-2 border-orange shadow-o3 rounded-xl h-80 my-10 p-2">
+            <div ref="left" class="px-6">
+                  <div v-for="project in projects"
+                        class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 border-2 border-orange shadow-o3 rounded-xl min-h-80 my-10 p-2">
                         <div class="border-2 border-orange-very text-orange-very p-2">
-                              <h4 class="font-bold uppercase text-2sxl text-center">
+                              <h4 class="font-bold uppercase text-2xl text-center">
                                     {{ project.name }}
                               </h4>
 
-                              <ul>
-                                    <li v-for="skill in project.skills">
+                              <ul class="my-2">
+                                    <li v-for="skill in project.skills"
+                                          class="text-sm text-bblack inline-block bg-orange-light mb-1 me-2 p-1 rounded-xl">
                                           {{ skill.name }}
+                                    </li>
+                              </ul>
+
+                              <ul>
+                                    Github repository:
+                                    <li v-for="link in project.github" class="underline">
+                                          <a :href="link">{{ link }}</a>
                                     </li>
                               </ul>
                         </div>
 
-                        <div class="border-2 border-orange-very text-orange-very p-2 flex items-center">
-                              {{ project.description }}
-                        </div>
-
                         <div class="border-2 border-orange-very flex items-center">
                               <video :src="project.video" controls></video>
+                        </div>
+
+                        <div
+                              class="border-2 border-orange-very md:col-span-2 lg:col-span-1 text-orange-very p-2 flex items-center">
+                              {{ project.description }}
                         </div>
                   </div>
             </div>
