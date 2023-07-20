3f7511de36d06ba842c294e80e68fde625243072
Pietro Giordano <giopi87@gmail.com>
2023-07-20 17:30:36 +0200

    fix

--- src/router.js
+++ src/router.js
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import AppHeader from './pages/AppHeader.vue';
-import AboutMe from './pages/AboutMe.vue';
-import Skills from './pages/Skills.vue';
-import Portfolio from './pages/Portfolio.vue';
-import Contact from './pages/Contact.vue';
+import AppHeader from './views/AppHeader.vue';
+import AboutMe from './views/AboutMe.vue';
+import Skills from './views/Skills.vue';
+import Portfolio from './views/Portfolio.vue';
+import Contact from './views/Contact.vue';
 
 const router = createRouter({
       history: createWebHistory(),
